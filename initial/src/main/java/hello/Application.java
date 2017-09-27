3e8a6a9500d724f05b3f6b0c8d0672f371eee435
leader80 <giovanni@degiorgi.me>
2017-09-27 21:28:36 +0200

    update

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -2,25 +2,32 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-	@RequestMapping("/")
-	public String home() {
-		return "Hello Docker World!!";
-	}
+  @RequestMapping("/")
+  public String home() {
+    return "Hello Docker World!!";
+  }
 
-	@RequestMapping("/debris")
-	public String debis() {
-		return "Debris algo";
-	}
+  @RequestMapping("/debris")
+  public String debis() {
+    return "Debris algo!!";
+  }
 
+  @RequestMapping("/req")
+  public String getAttr(@RequestParam String someAttr) {
+    return "ciao-" + someAttr;
+  }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 }
